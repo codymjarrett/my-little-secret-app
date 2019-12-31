4cea1b8d8dca61f4dd368b606fa321c5d0dea06c
Hero's Dad <cody.monta@gmail.com>
2019-12-31 13:02:36 -0600

    added data fetching, need to style the secret card

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import fetch from 'unfetch'
+import useSWR from 'swr'
 
 /* includes tailwind here */
 import '../styles/main.css'
@@ -7,27 +8,54 @@ import '../styles/main.css'
 import SecretButton from './../components/SecretButton'
 import ReactModal from '../components/ReactModal/ReactModal'
 
+const fetcher = async () => {
+	const data = await fetch('http://localhost:4000/api/secret')
+	const json = await data.json()
+	return {
+		posts: json.data,
+	}
+}
 const Home = () => {
-	useEffect(() => {
-		fetchData()
-	})
 	const [isOpen, setIsOpen] = useState(false)
+	const { data, error } = useSWR('/api/secrets', fetcher)
 
-	const fetchData = async () => {
-		const secrets = await axios.get('http://localhost:4000/api/secret')
-		console.log(secrets.data)
+	const renderData = () => {
+		if (error) return <div>Failed to load</div>
+		if (!data) return <div>Loading...</div>
+		return data.posts.map(({ secret, _id }) => {
+			return (
+				<div className="shadow-lg bg-white-300">
+					<div key={_id}>{secret}</div>
+				</div>
+			)
+		})
 	}
 
-	const toggleModal = () =>{
+	const toggleModal = () => {
 		setIsOpen(!isOpen)
 	}
 
 	return (
 		<>
-			<SecretButton toggleModal={toggleModal}/>
-			<ReactModal isOpen={isOpen} toggleModal={toggleModal}/>
+			<SecretButton toggleModal={toggleModal} />
+			{renderData()}
+			<ReactModal isOpen={isOpen} toggleModal={toggleModal} />
 		</>
 	)
 }
+
+/* 
+Keeping this here for future reference. Use this if you want initial data on build
+not dynamically on client 
+*/
+
+// Home.getInitialProps = async () => {
+// 	const data = await fetch('http://localhost:4000/api/secret')
+// 	const result = await data.json()
+
+// 	return {
+// 		posts: result.data
+// 	}
+// }
 
 export default Home
